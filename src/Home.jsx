05b66d74f3b610c65d88a28177790d2433c922a1
reactfrntdvlpr <reactfrntdvlpr@gmail.com>
2025-02-18 13:17:01 +0530

    Index page Update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect } from "react";
 import HeroSection from "./Component/HeroSection";
 import { useGLobalContext } from "./Context";
+import Services from "./Services";
+import Contact from "./Contact";
 
-const Home = () =>{
+const Home = () => {
     // const data = {
     //     name : "Tauseef ur Rahman",
     //     image : "./images/hero.svg",
     // };
 
-    const {updateHomepage} = useGLobalContext();
+    const { updateHomepage } = useGLobalContext();
 
     useEffect(() => updateHomepage(), []);
-    
 
-    return  <HeroSection />;
-       
-        
 
+    return (
+        <>
+            <HeroSection />
+            <Services />
+            <Contact />
+
+        </>
+
+
+
+    )
 };
 
 export default Home;
